62eff78d8b6d360e0857f7ae3cb7f6ee2329df0f
6opuc-dev <75522440+6opuc-dev@users.noreply.github.com>
2021-04-23 14:15:54 +0300

    Update

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,59 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+//      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+//      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+//      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+
+      Car car1 = new Car("BMW", 3);
+      car1.setUser(user1);
+      user1.setCar(car1);
+      Car car2 = new Car("Zhigul", 1);
+      user2.setCar(car2);
+      car2.setUser(user2);
+      Car car3 = new Car("Toyota", 2);
+      user3.setCar(car3);
+      car3.setUser(user3);
+
+      userService.add(user1);
+      userService.add(user2);
+      userService.add(user3);
+
+      List<User> usersBmw = userService.findUserByCar("BMW", 3);
+      for(User user : usersBmw) {
+         System.out.println("\n a user with an id " + user.getId() + " has a BMW series 3");
+      }
+
+//      List<User> users = userService.listUsers();
+//      for (User user : users) {
+//         System.out.println("Id = "+user.getId());
+//         System.out.println("First Name = "+user.getFirstName());
+//         System.out.println("Last Name = "+user.getLastName());
+//         System.out.println("Email = "+user.getEmail());
+//         System.out.println("Car = "+user.getCar().getModel());
+//         System.out.println();
+//      }
+
+      context.close();
+   }
+}
